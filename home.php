db289f846d006b0371ab832ab19eb71f43965b96
RubenCox <cox.ruben@hotmail.com>
2015-09-07 01:41:56 +0200

    Straffen

--- home.php
+++ home.php
@@ -24,8 +24,46 @@
 	<div class="container">
 		<div class="row mt centered">
 			<div class="col-lg-6 col-lg-offset-3">
-				<h1>Your Landing Page<br/>Looks Wonderful Now.</h1>
-				<h3>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</h3>
+				<h1>Beheren</h1>
+				<h3>Hier kan de admin schachten, inzet en evenementen beheren</h3>
+			</div>
+		</div><!-- /row -->
+		
+		<div class="row mt centered">
+			<div class="col-lg-4">
+				<a href="schachten.php"><img src="assets/img/ser01.png" width="180" alt=""></a>
+				<h4>1 - Schachten</h4>
+				<p>Beheer hier je schachten!</p>
+			</div><!--/col-lg-4 -->
+
+			<div class="col-lg-4">
+				<a href="inzet.php"><img src="assets/img/ser02.png" width="180" alt=""></a>
+				<h4>2 - Inzet</h4>
+				<p>Evalueer je schachten op Aanwezigheid, opdrachten en straffen!</p>
+
+			</div><!--/col-lg-4 -->
+
+			<div class="col-lg-4">
+				<a href="evenementen.php"><img src="assets/img/ser03.png" width="180" alt=""></a>
+				<h4>3 - Evenementen</h4>
+				<p>Beheer hier je evenementen!</p>
+
+			</div><!--/col-lg-4 -->
+			
+			<div class="col-lg-4">
+				<a href="straffen.php"><img src="assets/img/pic1.jpg" width="180" alt=""></a>
+				<h4>4 - Straffen</h4>
+				<p>Beheer de straffen + hun graad.</p>
+
+			</div><!--/col-lg-4 -->
+		</div><!-- /row -->
+	</div><!-- /container -->
+	
+	<div class="container">
+		<div class="row mt centered">
+			<div class="col-lg-6 col-lg-offset-3">
+				<h1>Beheren</h1>
+				<h3>Hier kan de admin schachten, inzet en evenementen beheren</h3>
 			</div>
 		</div><!-- /row -->
 		
